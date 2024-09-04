d695e9103138aaff7b1539681deb9d22d4113485
Usman Ali Khan <usmanali23515@gmail.com>
2024-09-04 20:14:02 +0500

    adding navbar in fetching card by Api

--- 08CardsByFetchingApi/src/App.jsx
+++ 08CardsByFetchingApi/src/App.jsx
@@ -1,9 +1,9 @@
 import { useState , useEffect} from 'react'
 import './App.css'
-
+import Navbar from './components/Navbar'
 function App() {
   const [data, setData] = useState([])
-
+ 
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/posts')
     .then(response=>response.json())
@@ -13,11 +13,12 @@ function App() {
  
   return (
     <>
-        {
-        data.map((info)=>(<div key={info.id} style={cardStyle}>
-          <img src={`https://images.unsplash.com/photo-1546961329-78bef0414d7c?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHVzZXJ8ZW58MHx8MHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=60`}
+       <Navbar/> 
+        <div className='container'>
+        {data.map((info)=>(<div key={info.id} className='card'>
+          <img className='grl'src={`https://images.unsplash.com/photo-1546961329-78bef0414d7c?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHVzZXJ8ZW58MHx8MHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=60`}
             alt="Random"
-          style={imageStyle}
+          
             />
           <h2>
              {info.title}
@@ -25,34 +26,12 @@ function App() {
            <p>
            {info.body}</p>
             </div>
-            ))
-        }
+            )) }
+        </div>
+       
     </>
   )
 }
-const cardStyle = {
-  display: 'inline-block',
-  boxSizing:'border-box',
-  // position: 'relative',
-  border: '1px solid #ddd',
-  borderRadius: '8px',
-  padding: '20px',
-  margin: '10px',
-  width: '300px',
-  height: '600px',
-  boxShadow: '4px 4px 8px rgba(0,0,0,0.1)',
-};
-const imageStyle = {
 
-  width: '240px',
-  height: '200px',
-  objectFit: 'cover', // Ensures the image covers the area without distortion
-  borderRadius: '8px',
-  marginTop: "0px",
-  marginBottom: '0px',
-  // marginLeft: "10px",
-  // marginRight: "220px",
-
-
-};
+ 
 export default App
---
+++ 08CardsByFetchingApi/src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>Deprtments</li>
+                <li>About us</li>
+                <li>Contact us</li>
+            </ul>
+        </nav>
+      
+    </div>
+  )
+}
+
+export default Navbar
