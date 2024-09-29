2feaafcb7e78d487243d82867224066f65a3dcdb
Usman Ali Khan <usmanali23515@gmail.com>
2024-09-30 02:48:27 +0500

    adding different components

--- 19megaBlog/src/components/index.js
+++ 19megaBlog/src/components/index.js
@@ -1,8 +1,9 @@
 import Footer from "./Footer/Footer";
 import Header from "./Header/Header";
 import Logo from "./Logo";
-import Container from "./container/Container"
-import LogoutBtn from "./Header/LogoutBtn"
+import Container from "./container/Container";
+import LogoutBtn from "./Header/LogoutBtn";
+import Input from "./Input";
+import Button from "./Button";
 
-
-export {Header , Footer ,Container , Logo , LogoutBtn  }
+export { Header, Footer, Container, Logo, LogoutBtn, Input, Button }
